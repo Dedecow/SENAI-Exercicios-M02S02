f8c87de610407f9023a0483c971459e086073a73
Andre <andrehbahia@gmail.com>
2023-09-22 19:49:33 -0300

    commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class App public class Main {
+    private String cpf, nome, sobrenome, email, celular, nomeDaMae;
+
+    //construtor
+    public PessoaFisica (String cpf){
+        this.cpf=cpf;
+        this.nome = nome;
     }
+
 }
